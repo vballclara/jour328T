d41ab252dffb8625fb3358263188ea78840582df
vballclara <60446368+vballclara@users.noreply.github.com>
2022-04-11 18:37:12 -0400

    change data

--- lab_10/javascript.js
+++ lab_10/javascript.js
@@ -2,10 +2,10 @@ var ctx = document.getElementById('myChart').getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ['2015', '2016', '2017', '2018', '2019', '2020'],
+        labels: ['America', 'East Asia & Pacific', 'Europe & Central Asia', 'Middle East and North Africa', 'South Asia', 'Sub-Saharan Africa'],
         datasets: [{
-            label: '# of Apples',
-            data: [12, 19, 3, 5, 2, 3],
+            label: 'Average of GDP per Capita',
+            data: [18791.13, 14480.3, 20818.45, 13856, 2505.17, 2569],
             backgroundColor: [
                 '#ff0800'
             ]
