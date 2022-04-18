5f737971f4a0f2c4ba2743182797519a7f867a74
vballclara <60446368+vballclara@users.noreply.github.com>
2022-04-18 19:46:34 -0400

    change values

--- lab_11/line_chart/javascript.js
+++ lab_11/line_chart/javascript.js
@@ -5,19 +5,78 @@ var myChart = new Chart(ctx, {
     data: {
         labels: ['2015', '2016', '2017', '2018', '2019', '2020'],
         datasets: [{
-            label: '# of Apples',
-            data: [12, 19, 3, 5, 2, 3],
+            label: '8-year-olds',
+            data: [1, 4, 0, 0, 0, 0],
             fill: true,
             borderColor: 'rgba(255, 8, 0)',
             backgroundColor: 'rgba(255, 8, 0, 0.1)',
             tension: 0.1},
         {
-            label: '# of Oranges',
-            data: [22, 10, 23, 6, 22, 13],
+            label: '9-year-olds',
+            data: [0, 0, 0, 0, 0, 0],
             fill: true,
             borderColor: 'rgba(255, 165, 0)',
             backgroundColor: 'rgba(255, 165, 0, 0.1)',
-            tension: 0.1}
+            tension: 0.1},
+        {
+            label: '10-year-olds',
+            data: [0, 0, 1, 0, 2, 0],
+            fill: true,
+            borderColor: 'rgba(255, 8, 0)',
+            backgroundColor: 'rgba(255, 8, 0, 0.1)',
+            tension: 0.1},
+        {
+            label: '11-year-olds',
+            data: [0, 2, 1, 0, 1, 2],
+            fill: true,
+            borderColor: 'rgba(255, 8, 0)',
+            backgroundColor: 'rgba(255, 8, 0, 0.1)',
+            tension: 0.1},
+        {
+            label: '12-year-olds',
+            data: [1, 0, 2, 1, 1, 0],
+            fill: true,
+            borderColor: 'rgba(255, 8, 0)',
+            backgroundColor: 'rgba(255, 8, 0, 0.1)',
+            tension: 0.1},
+        {
+            label: '13-year-olds',
+            data: [1, 1, 6, 0, 2, 1],
+            fill: true,
+            borderColor: 'rgba(255, 8, 0)',
+            backgroundColor: 'rgba(255, 8, 0, 0.1)',
+            tension: 0.1},
+        {
+            label: '14-year-olds',
+            data: [10, 4, 8, 4, 8, 3],
+            fill: true,
+            borderColor: 'rgba(255, 8, 0)',
+            backgroundColor: 'rgba(255, 8, 0, 0.1)',
+            tension: 0.1},
+        {
+            label: '15-year-olds',
+            data: [22, 12, 19, 37, 32, 21],
+            fill: true,
+            borderColor: 'rgba(255, 8, 0)',
+            backgroundColor: 'rgba(255, 8, 0, 0.1)',
+            tension: 0.1},
+
+        {
+            label: '16-year-olds',
+            data: [244, 193, 202, 356, 377, 283],
+            fill: true,
+            borderColor: 'rgba(255, 8, 0)',
+            backgroundColor: 'rgba(255, 8, 0, 0.1)',
+            tension: 0.1},
+
+        {
+            label: '17-year-olds',
+            data: [519, 540, 444, 524, 526, 446], 
+            fill: true,
+            borderColor: 'rgba(255, 8, 0)',
+            backgroundColor: 'rgba(255, 8, 0, 0.1)',
+            tension: 0.1},
+
         ]
     },
     options: {
